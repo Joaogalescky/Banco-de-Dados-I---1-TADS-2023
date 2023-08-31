644192588e6d972ad87ba99d4a1560ec8e24104f
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2023-08-30 23:03:05 -0300

    Update Exercicio_Gatilho - João Vitor & João Victor.sql

--- Exercicio_Gatilho - João Vitor & João Victor.sql
+++ Exercicio_Gatilho - João Vitor & João Victor.sql
@@ -11,7 +11,7 @@ create table USUARIO(
 );
 
 insert into USUARIO(Nome_usuario, Telefone) values
-("João Pedro", "45984563215");
+('João Pedro', '45984563215');
 
 select * from USUARIO;
 
@@ -21,15 +21,15 @@ create table AUTOR(
     Sobrenome varchar (40) not null,
     Nacionalidade varchar (40) null,
     Estilo varchar (40) null,
-    primary key (id_autor)
+    primary key (id_Autor)
 );
 
 insert into AUTOR(Nome, Sobrenome, Nacionalidade, Estilo) values
-("Machado", "de Assis", "brasileira", "romance"),
-("Karl", "Marx", "alemã", "filósofia"),
-("Max", "Weber", "alemã", "sociologia"),
-("Adam", "Smith", "escocesa", "filósofia"),
-("Émile", "Durkheim", "francesa", "sociologia");
+('Machado', 'de Assis', 'brasileira', 'romance'),
+('Karl', 'Marx', 'alemã', 'filósofia'),
+('Max', 'Weber', 'alemã', 'sociologia'),
+('Adam', 'Smith', 'escocesa', 'filósofia'),
+('Émile', 'Durkheim', 'francesa', 'sociologia');
 
 select * from AUTOR;
 
@@ -39,22 +39,22 @@ create table LIVROS(
     Estilo varchar (50) not null,
     Ano int (4) not null,
     Disponibilidade varchar (25) not null,
-    id_Autor int not null,
+    Autor_id int not null,
 	primary key (id_Livro),
-	constraint FK_Autor foreign key (id_Autor) references AUTOR(id_Autor)
+	constraint FK_Autor foreign key (Autor_id) references AUTOR(id_Autor)
 );
 
-insert into LIVROS(Titulo, Ano, Estilo, id_Autor, Disponibilidade) values
-("Dom Casmurro", 1899, "romance", 1, "disponivel"),
-("Memórias Póstulas de Brás Cubas", 1881, "romance", 1, "disponivel"),
-("O Capital", 1887, "filósofia", 2, "disponivel"),
-("Uma Contribuição para a Crítica da Economia Política", 1850, "filósofia", 2, "disponivel"),
-("A Ética Protestante", 1904, "filósofia", 3, "disponivel"),
-("O Político e o Cientista", 1919, "filósofia", 3, "disponivel"),
-("A riqueza das nações", 1776, "filósofia", 4, "disponivel"),
-("A mão invisível", 1759, "filósofia", 4, "disponivel"),
-("Da Divisão do Trabalho Social", 1893, "sociologia", 5, "disponivel"),
-("As Regras do Método Sociológico", 1895, "sociologia", 5, "disponivel");
+insert into LIVROS(Titulo, Ano, Estilo, Autor_id, Disponibilidade) values
+('Dom Casmurro', 1899, 'romance', 1, 'disponivel'),
+('Memórias Póstulas de Brás Cubas', 1881, 'romance', 1, 'disponivel'),
+('O Capital', 1887, 'filósofia', 2, 'disponivel'),
+('Uma Contribuição para a Crítica da Economia Política', 1850, 'filósofia', 2, 'disponivel'),
+('A Ética Protestante', 1904, 'filósofia', 3, 'disponivel'),
+('O Político e o Cientista', 1919, 'filósofia', 3, 'disponivel'),
+('A riqueza das nações', 1776, 'filósofia', 4, 'disponivel'),
+('A mão invisível', 1759, 'filósofia', 4, 'disponivel'),
+('Da Divisão do Trabalho Social', 1893, 'sociologia', 5, 'disponivel'),
+('As Regras do Método Sociológico', 1895, 'sociologia', 5, 'disponivel');
 
 select * from LIVROS;
 
@@ -62,17 +62,12 @@ create table EMPRESTIMOS(
 	id_Emprestimo int not null auto_increment,
     Data_emprestimo date not null,
     Data_devolucao date null,
-    idUsuario int not null,
-    idLivro int not null,
+    Usuario_id int not null,
+    Livro_id int not null,
     primary key (id_Emprestimo),
-    constraint FK_Usuario_Emprestimo foreign key (idUsuario) references USUARIO (id_Usuario),
-    constraint FK_Livro_Emprestimo foreign key (idLivro) references LIVROS (id_Livro)
+    constraint FK_Usuario_Emprestimo foreign key (Usuario_id) references USUARIO (id_Usuario),
+    constraint FK_Livro_Emprestimo foreign key (Livro_id) references LIVROS (id_Livro)
 );
-
-insert into EMPRESTIMOS (Data_emprestimo, idUsuario, idLivro) values
-(2023-08-25, 1, 2);
-
-select * from EMPRESTIMOS;
 
 -- Gatilho (Trigger) status "emprestado"
 DELIMITER $ 
@@ -80,8 +75,9 @@ create trigger TGR_Emprestimo_Emprestado after insert
 on EMPRESTIMOS
 for each row
 begin
-	update LIVROS set Disponibilidade = "emprestado"
-    where id_Livro = NEW.EMPRESTIMOS; 
+	update LIVROS set Disponibilidade = 'emprestado'
+    where id_Livro = NEW.Livro_id;
+    -- NEW.idLivro está referenciando o livro que está sendo emprestado
 end$
 DELIMITER ;
 
@@ -91,9 +87,22 @@ create trigger TGR_Emprestimo_Disponivel after insert
 on EMPRESTIMOS
 for each row
 begin
-	update LIVROS set Disponibilidade = "disponivel"
-    where Data_devolucao = NEW.EMPRESTIMOS;
+	update LIVROS set Disponibilidade = 'disponivel'
+    where id_Livro = NEW.Livro_id;
     -- Ideia --> Alterar de "emprestado" para "disponivel" quando for detectado o preenchimento da Data_devolucao
 end$
 DELIMITER ;
 
+-- Inserção de empréstimo
+insert into EMPRESTIMOS (Data_emprestimo, Usuario_id, Livro_id) values
+('2023-08-25', 1, 2);
+
+select * from EMPRESTIMOS;
+
+-- Atualização da data de devolução
+update EMPRESTIMOS set Data_devolucao = '2023-09-01'
+where Usuario_id = 1 and Livro_id = 2;
+
+select * from LIVROS;
+
+select * from EMPRESTIMOS;
