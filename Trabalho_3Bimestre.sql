1b5c3ab8e835eb3910ca8ec8b5514cc1fb0de663
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2023-09-20 00:24:19 -0300

    Update Trabalho_3Bimestre.sql

--- Trabalho_3Bimestre.sql
+++ Trabalho_3Bimestre.sql
@@ -18,12 +18,11 @@ d)    Um procedimento para FINALIZAR a participação de um jogador em uma parti
 e)    Um gatilho (trigger) para atualizar o número de jogadores ao finalizar a participação de um jogador em uma partida. --> CERTO
 */
 
-create database Trabalho_3°Bimestre;
-use Trabalho_3°Bimestre;
+CREATE DATABASE Trabalho_3°Bimestre;
+USE Trabalho_3°Bimestre;
 
-create table Partida(
+CREATE TABLE Partida(
 	id_partida int not null auto_increment,
-    jogador_id int not null,
     nome_jogo varchar (100) not null,
     fornecedor_jogo varchar (50) not null,
     hora_inicio time not null,
@@ -32,46 +31,54 @@ create table Partida(
     primary key (id_partida)
 );
 
-create table Jogador(
+CREATE TABLE Jogador(
 	id_jogador int not null auto_increment,
     nome_jogador varchar(50) not null,
     nivel_jogador int not null,
     primary key (id_jogador)
 );
 
-create table JogandoPartida(
+CREATE TABLE JogandoPartida(
 	jogador_id int not null,
 	partida_id int not null,
-    horario_inicio time not null,
+    horario_inicio time null,
     horario_fim time null,
 	constraint FK_Partida foreign key (partida_id) references Partida(id_partida),
     constraint FK_Jogador foreign key (jogador_id) references Jogador(id_jogador)
 );
 
--- VIEW -- a)
-create view VW_Jogador_Partida
-as select Jogador.nome_jogador as Jogador, Partida.id_partida as Partida_Jogada
-from Jogador inner join Partida
-on Jogador.id_jogador = Partida.jogador_id;
-
-select * from VW_Jogador_Partida
+CREATE VIEW VW_Jogador_Partida AS SELECT
+Jogador.nome_jogador AS Jogador,
+Partida.id_partida AS ID_Partida,
+Partida.nome_jogo AS Partida,
+Partida.hora_inicio AS Horario_de_inicio,
+Partida.hora_fim AS Horario_de_terminio
+FROM Jogador INNER JOIN JogandoPartida ON Jogador.id_jogador = JogandoPartida.jogador_id
+INNER JOIN Partida ON JogandoPartida.partida_id = Partida.id_partida
 
 -- PROCEDURE -- b)
 DELIMITER $
-create procedure inserir_jogador (
-IN i_jogador_id int, -- Declara um parâmetro de entrada
+CREATE PROCEDURE inserir_jogador (
+-- Declara um parâmetro de entrada
+-- Utiliza-se esses parâmetros para atualizar a tabela JogandoPartida
+IN i_jogador_id int,
 IN i_partida_id int,
 IN i_hora_entrada time)
 BEGIN
-	UPDATE JogandoPartida SET horario_inicio = i_hora_entrada
-    WHERE jogador_id = i_jogador_id AND partida_id = i_partida_id;
+	IF NOT EXISTS (
+		SELECT * FROM JogandoPartida
+        WHERE jogador_id = i_jogador_id AND partida_id = i_partida_id
+    ) THEN
+		INSERT INTO JogandoPartida (jogador_id, partida_id, horario_inicio) VALUES
+		(i_jogador_id, i_partida_id, i_hora_entrada);
+    END IF;
 END $
 DELIMITER ;
 
 -- TRIGGER -- c)
 DELIMITER $
-create trigger TGR_atualizar_jogadores after insert on JogandoPartida
-for each row
+CREATE TRIGGER TGR_atualizar_jogadores AFTER INSERT ON JogandoPartida
+FOR EACH ROW
 BEGIN
 	UPDATE Partida SET num_jogadores = num_jogadores + 1
     WHERE id_partida = NEW.partida_id;
@@ -84,21 +91,29 @@ DELIMITER ;
     "Encapsular" a lógica de atualização em uma unidade reutilizável no BD.
 */
 DELIMITER $
-create procedure finalizar_participação_jogador (
-IN p_jogador_id int, -- Declara um parâmetro de entrada
+CREATE PROCEDURE finalizar_participação_jogador (
+IN p_jogador_id int,
 IN p_partida_id int,
 IN p_hora_saida time)
--- Utiliza-se esses parâmetros para atualizar a tabela JogandoPartida
 BEGIN
-	UPDATE JogandoPartida SET horario_fim = p_hora_saida
-    WHERE jogador_id = p_jogador_id AND partida_id = p_partida_id;
+-- Verificar se o jogador esta participando da partida
+    IF EXISTS (
+        SELECT * FROM JogandoPartida
+        WHERE jogador_id = p_jogador_id AND partida_id = p_partida_id
+    ) THEN
+        -- Atualiza o horário de saída do jogador na partida
+        UPDATE JogandoPartida
+        SET horario_fim = p_hora_saida
+        WHERE jogador_id = p_jogador_id AND partida_id = p_partida_id;
+        -- Atualiza o número de jogadores na partida
+    END IF;
 END $
 DELIMITER ;
 
 -- TRIGGER -- e)
 DELIMITER $
-create trigger TGR_atualizar_jogadores_final_partida after update on JogandoPartida
-for each row
+CREATE TRIGGER TGR_atualizar_jogadores_final_partida AFTER UPDATE ON JogandoPartida
+FOR EACH ROW
 BEGIN
 	UPDATE Partida SET num_jogadores = num_jogadores - 1
     WHERE id_partida = NEW.partida_id;
@@ -106,33 +121,33 @@ END $
 DELIMITER ;
 
 -- insert
-insert into Partida (nome_jogo, fornecedor_jogo, hora_inicio, hora_fim, num_jogadores, jogador_id) values
-('Corinthians vs Bota Fogo', 'Adidas', '12:30:00', '14:00:00', 0, 1),
-('Flamengo vs Internacional', 'Nike', '10:00:00', '11:20:00', 0, 2);
+INSERT INTO Partida (nome_jogo, fornecedor_jogo, hora_inicio, hora_fim, num_jogadores) values
+('Corinthians vs Bota Fogo', 'Adidas', '12:30:00', '14:00:00', 0),
+('Flamengo vs Internacional', 'Nike', '10:00:00', '11:20:00', 0);
 
-insert into Jogador (nome_jogador, nivel_jogador) values
+INSERT INTO Jogador (nome_jogador, nivel_jogador) values
 ('Jô', 11),
-('Matheus', 9);
-
--- Inserir um jogador (com ID 1) em uma partida (com ID 1) com horário de entrada e saída
-INSERT INTO JogandoPartida (partida_id, jogador_id, horario_inicio)
-VALUES (1, 1);
-
--- Inserir outro jogador (com ID 2) em outra partida (com ID 2) com horário de entrada e saída
-INSERT INTO JogandoPartida (partida_id, jogador_id, horario_inicio)
-VALUES (2, 2);
+('Matheus', 9),
+('Pedro', 10),
+('Guilherme', 5);
 
 -- select
-select * from Partida;
+SELECT * FROM VW_Jogador_Partida;
 
-select * from Jogador;
+SELECT * FROM Partida;
 
-select * from JogandoPartida;
+SELECT * FROM Jogador;
 
--- Chamando procedimento b)
-CALL inserir_jogador(1);
-CALL inserir_jogador(2);
+SELECT * FROM JogandoPartida;
 
--- Chamando procedimento d)
-CALL finalizar_participação_jogador(1, 1, '13:30:00');
-CALL finalizar_participação_jogador(2, 2, '11:45:00');
+-- Chamando procedimento b) - inserir jogadores
+CALL inserir_jogador(1, 1, '13:30:00');
+CALL inserir_jogador(2, 1, '13:00:00');
+CALL inserir_jogador(3, 2, '10:00:00');
+CALL inserir_jogador(4, 2, '10:50:00');
+
+-- Chamando procedimento d) - finalizar a participação dos jogadores
+CALL finalizar_participação_jogador(1, 1, '14:00:00');
+CALL finalizar_participação_jogador(2, 1, '13:45:00');
+CALL finalizar_participação_jogador(3, 2, '11:00:00');
+CALL finalizar_participação_jogador(4, 2, '11:20:00');
